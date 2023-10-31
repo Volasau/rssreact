9a046224771c3a33789dbd5973eb647bcbf8b3f3
Ryhor Volsasu <g.volosov@email.com>
2023-10-31 15:45:05 +0400

    refactory: change place for ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import './App.css';
 import Body from './components/body/Body';
-import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
 import ErrorButton from './components/errorBoundary/btnTest';
 import Header from './components/header/Header';
 
 function App() {
   return (
-    <ErrorBoundary>
-      <div className="wrapper">
-        <Header />
-        <Body />
-        <ErrorButton />
-      </div>
-    </ErrorBoundary>
+    <div className="wrapper">
+      <Header />
+      <Body />
+      <ErrorButton />
+    </div>
   );
 }
 
