08953c66ae20955db7ea66b6ffd8c86aef2c5a90
Ryhor Volsasu <g.volosov@email.com>
2023-10-31 20:26:24 +0400

    refactor: change class Header to function Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
 import './header.css';
 import Search from '../form/Form';
 import Logo from '../../../public/logo.png';
 
-class Header extends Component {
-  render(): React.ReactNode {
-    return (
-      <div className="header">
-        <img src={Logo} className="header__logo"></img>
-        <Search />
-      </div>
-    );
-  }
+function Header() {
+  return (
+    <div className="header">
+      <img src={Logo} className="header__logo" alt="Logo" />
+      <Search />
+    </div>
+  );
 }
 
 export default Header;
