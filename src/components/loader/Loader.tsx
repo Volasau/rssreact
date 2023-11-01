cc86a9783a9f8b9bfeaea067f659eaa00a86e2d7
Ryhor Volsasu <g.volosov@email.com>
2023-11-01 09:34:28 +0400

    refactor: change class Loader to function Loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
 import Loaderimg from '../../../public/loader.png';
 
-class Loader extends Component {
-  render(): React.ReactNode {
-    return (
-      <div className="loader">
-        <img src={Loaderimg} alt="Loader" className="loader__img" />
-      </div>
-    );
-  }
+function Loader() {
+  return (
+    <div className="loader">
+      <img src={Loaderimg} alt="Loader" className="loader__img" />
+    </div>
+  );
 }
 
 export default Loader;
