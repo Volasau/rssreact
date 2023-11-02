970b4401f74669f42ece942f611de59a1d1a1a5c
Ryhor Volsasu <g.volosov@email.com>
2023-11-02 12:52:27 +0400

    refactor: change class ApiService to function getPlanets

--- src/Api/fetch.tsx
+++ src/Api/fetch.tsx
@@ -1,20 +1,18 @@
-class ApiService {
-  static async getPlanets(searchQuery: string | null) {
-    let url = 'https://swapi.dev/api/planets';
+async function getPlanets(searchQuery: string | null) {
+  let url = 'https://swapi.dev/api/planets';
 
-    if (searchQuery) {
-      url = `https://swapi.dev/api/planets/?search=${searchQuery}`;
-    }
+  if (searchQuery) {
+    url = `https://swapi.dev/api/planets/?search=${searchQuery}`;
+  }
 
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-    if (data.results) {
-      return data.results;
-    } else {
-      throw new Error('Not data "results"');
-    }
+  if (data.results) {
+    return data.results;
+  } else {
+    throw new Error('Not data "results"');
   }
 }
 
-export default ApiService;
+export default getPlanets;
