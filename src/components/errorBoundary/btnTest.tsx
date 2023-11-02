5107771c945319199e5c1f314621748166a3f35b
Ryhor Volsasu <g.volosov@email.com>
2023-11-02 12:55:48 +0400

    refactor: change class ErrorButton to function BodyErrorButton

--- src/components/errorBoundary/btnTest.tsx
+++ src/components/errorBoundary/btnTest.tsx
@@ -1,34 +1,24 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import './btnTest.css';
 
-interface State {
-  hasError: boolean;
-}
+function ErrorButton() {
+  const [hasError, sethasError] = useState(false);
 
-class ErrorButton extends Component<object, State> {
-  constructor(props: object | Readonly<object>) {
-    super(props);
-    this.state = {
-      hasError: false,
-    };
-  }
-
-  handleButtonClick = () => {
-    this.setState({ hasError: true });
+  const handleButtonClick = () => {
+    sethasError(true);
   };
 
-  render() {
-    if (this.state.hasError) {
-      throw new Error('I crashed!');
-    }
-    return (
-      <div className="error__test">
-        <button onClick={this.handleButtonClick} className="error__btn">
-          ERROR
-        </button>
-      </div>
-    );
+  if (hasError) {
+    throw new Error('I crashed!');
   }
+
+  return (
+    <div className="error__test">
+      <button onClick={handleButtonClick} className="error__btn">
+        ERROR
+      </button>
+    </div>
+  );
 }
 
 export default ErrorButton;
