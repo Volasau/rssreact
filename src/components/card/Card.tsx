877c38bbecf287ac69acfff1dc74107d0988f8c5
Ryhor Volsasu <g.volosov@email.com>
2023-11-06 01:50:33 +0400

    feat: add componante modal

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import './card.css';
+import Modal from '../modal/modal';
 
 export interface CardProps {
   name: string;
@@ -8,27 +10,38 @@ export interface CardProps {
   url: string;
 }
 
-const Card = (props: CardProps) => {
-  const handleCardClick = () => {};
+const Card: React.FC<CardProps> = (props: CardProps) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleCardClick = () => {
+    setIsModalVisible(true);
+  };
 
   return (
-    <div className="card__container" onClick={handleCardClick}>
-      <div className="card__title text">
-        🌌
-        <span className="card__span-title">{props.name}</span>
+    <>
+      <div className="card__container" onClick={handleCardClick}>
+        <div className="card__title text">
+          🌌
+          <span className="card__span-title">{props.name}</span>
+        </div>
+        <div className="card__climate text">
+          <span className="card__span">climate: </span> {props.climate}
+        </div>
+        <div className="card__terrain text">
+          <span className="card__span">terrain: </span>
+          {props.terrain}
+        </div>
+        <div className="card__population text">
+          <span className="card__span">population: </span>
+          {props.population}
+        </div>
       </div>
-      <div className="card__climate text">
-        <span className="card__span">climate: </span> {props.climate}
-      </div>
-      <div className="card__terrain text">
-        <span className="card__span">terrain: </span>
-        {props.terrain}
-      </div>
-      <div className="card__population text">
-        <span className="card__span">population: </span>
-        {props.population}
-      </div>
-    </div>
+      <Modal
+        active={isModalVisible}
+        setActive={setIsModalVisible}
+        cardData={props}
+      />
+    </>
   );
 };
 
