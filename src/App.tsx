22c3d074b65c11df3fd2295c2736cf6a317e2646
Ryhor Volsasu <g.volosov@email.com>
2023-11-11 16:07:55 +0400

    feat: add context for Body and App conponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Body from './components/body/Body';
 import ErrorButton from './components/errorBoundary/btnTest';
 import Header from './components/header/Header';
+import {
+  PlanetContext,
+  planetContextState,
+} from './components/context/Context';
+import { CardProps } from './components/card/Card';
 
 function App() {
+  const [planetData, setPlanetData] = useState<CardProps[]>(
+    planetContextState.planetData
+  );
+
   return (
     <Router>
       <div className="wrapper">
         <Header />
-        <Body />
+        <PlanetContext.Provider value={{ planetData, setPlanetData }}>
+          <Body />
+        </PlanetContext.Provider>
         <ErrorButton />
       </div>
     </Router>
--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './body.css';
 import { CardProps } from '../card/Card';
 import getPlanets, { getAllPlanets } from '../../Api/fetch';
@@ -6,12 +6,14 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import Select from '../select/Select';
 import Pagination from '../pagination/Pagination';
 import CardList from '../cardList/CardList';
+import { PlanetContext, PlanetContextType } from '../context/Context';
 
 function Body() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const [data, setData] = useState<CardProps[]>([]);
+  const { planetData, setPlanetData } =
+    useContext<PlanetContextType>(PlanetContext);
   const [isLoading, setIsLoading] = useState(false);
   const [noResults, setNoResults] = useState(false);
   const [page, setPage] = useState(1);
@@ -19,6 +21,48 @@ function Body() {
   const [selectedLimit, setSelectedLimit] = useState(10);
 
   useEffect(() => {
+    const getSearch = (
+      searchQuery: string,
+      page: number,
+      selectedLimit: number
+    ): void => {
+      setIsLoading(true);
+      setNoResults(false);
+
+      if (selectedLimit === 10) {
+        getPlanets(searchQuery, page)
+          .then((data) => {
+            setIsLoading(false);
+            setPlanetData(data.results);
+            setPage(page);
+            setTotalPages(Math.ceil(data.count / data.results.length));
+            setNoResults(data.results.length === 0);
+          })
+          .catch((error) => {
+            setIsLoading(false);
+            console.error('Error data:', error);
+          });
+      } else if (selectedLimit === 60) {
+        getAllPlanets(searchQuery)
+          .then((data) => {
+            const cardData: CardProps[] = data.map((planet) => ({
+              name: planet.name,
+              climate: planet.climate,
+              terrain: planet.terrain,
+              population: planet.population,
+              url: planet.url,
+            }));
+            setIsLoading(false);
+            setPlanetData(cardData);
+            setNoResults(cardData.length === 0);
+          })
+          .catch((error) => {
+            setIsLoading(false);
+            console.error('Error data:', error);
+          });
+      }
+    };
+
     const savedQuery = localStorage.getItem('savedSearchQuery');
     const urlParams = new URLSearchParams(location.search);
     const pageParam = parseInt(urlParams.get('page') || '1');
@@ -28,50 +72,15 @@ function Body() {
     } else {
       getSearch('', pageParam, selectedLimit);
     }
-  }, [location.search, selectedLimit]);
-
-  function getSearch(
-    searchQuery: string,
-    page: number,
-    selectedLimit: number
-  ): void {
-    setIsLoading(true);
-    setNoResults(false);
-
-    if (selectedLimit === 10) {
-      getPlanets(searchQuery, page)
-        .then((data) => {
-          setIsLoading(false);
-          setData(data.results);
-          setPage(page);
-          setTotalPages(Math.ceil(data.count / data.results.length));
-          setNoResults(data.results.length === 0);
-        })
-        .catch((error) => {
-          setIsLoading(false);
-          console.error('Error data:', error);
-        });
-    }
-    if (selectedLimit === 60) {
-      getAllPlanets(searchQuery)
-        .then((data) => {
-          const cardData: CardProps[] = data.map((planet) => ({
-            name: planet.name,
-            climate: planet.climate,
-            terrain: planet.terrain,
-            population: planet.population,
-            url: planet.url,
-          }));
-          setIsLoading(false);
-          setData(cardData);
-          setNoResults(cardData.length === 0);
-        })
-        .catch((error) => {
-          setIsLoading(false);
-          console.error('Error data:', error);
-        });
-    }
-  }
+  }, [
+    location.search,
+    selectedLimit,
+    setPlanetData,
+    setPage,
+    setTotalPages,
+    setIsLoading,
+    setNoResults,
+  ]);
 
   const handleNextPage = () => {
     const newPage = page + 1;
@@ -94,7 +103,11 @@ function Body() {
 
   return (
     <div className="wrapper">
-      <CardList isLoading={isLoading} noResults={noResults} data={data} />
+      <CardList
+        isLoading={isLoading}
+        noResults={noResults}
+        planetValue={{ planetData, setPlanetData }}
+      />
       <Pagination
         page={page}
         totalPages={totalPages}
