136e4bc1912143c429b5d652293e81bee1a0f15e
Ryhor Volsasu <g.volosov@email.com>
2023-11-30 02:09:07 +0400

    feat: add FormHook page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Error from './pages/Error/Error';
+import FormHook from './pages/FormHook/FormHook';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/formhook" element={<FormHook />} />
 
       <Route path="*" element={<Error />} />
     </Routes>
---
+++ src/pages/FormHook/FormHook.tsx
@@ -0,0 +1,13 @@
+import style from './FormHook.module.css';
+
+function FormHook() {
+  return (
+    <>
+      <div className={style.title}>
+        <h3 className={style.text}>FormHook</h3>
+      </div>
+    </>
+  );
+}
+
+export default FormHook;
