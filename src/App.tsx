d9f7c5d7c1e27bc75293c3831cc147745813dbf1
Ryhor Volsasu <g.volosov@email.com>
2023-11-30 02:10:46 +0400

    feat: add FormUn page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Error from './pages/Error/Error';
 import FormHook from './pages/FormHook/FormHook';
+import FormUN from './pages/FormUn/FormUn';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/formhook" element={<FormHook />} />
-
+      <Route path="/formun" element={<FormUN />} />
       <Route path="*" element={<Error />} />
     </Routes>
   );
---
+++ src/pages/FormUn/FormUn.tsx
@@ -0,0 +1,13 @@
+import style from './FormUN.module.css';
+
+function FormUN() {
+  return (
+    <>
+      <div className={style.title}>
+        <h3 className={style.text}>FormUN</h3>
+      </div>
+    </>
+  );
+}
+
+export default FormUN;
