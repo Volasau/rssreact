000a100c40c7af1612d1ac59062bf0ae48cc72a3
Ryhor Volsasu <g.volosov@email.com>
2023-11-30 01:40:33 +0400

    feat: add Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Error from './pages/Error/Error';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import style from './Home.module.css';
+
+function Home() {
+  return (
+    <>
+      <div className={style.title}>
+        <h3 className={style.text}>Forms HOME</h3>
+      </div>
+    </>
+  );
+}
+
+export default Home;
